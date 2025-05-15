e8b21cd328f8062408bb20dd95c2005cb1a682f9
Soyeon Cha <89539031+Soyeon-Cha@users.noreply.github.com>
2025-05-15 18:13:19 +0900

    Create 6_D.cpp

---
+++ week6/6_D.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int t, n, m;
+
+int main(){
+    cin >> t;
+
+    while(t--){
+        int ans = 0;
+
+        cin >> n >> m;
+        vector<int> a(n), b(m);
+
+        for(int i = 0; i < n; i++) cin >> a[i]; 
+        for(int i = 0; i < m; i++) cin >> b[i]; 
+
+        sort(a.begin(), a.end());
+        sort(b.begin(), b.end());
+
+        for(int i=0; i<n; i++){
+            auto pos = lower_bound(b.begin(), b.end(), a[i]);
+            ans += (int)(pos - b.begin());
+        }
+        cout << ans << "\n";
+    }
+}
