5cbb11f890eb95b52ab0aa89c064f89442311b80
Soyeon Cha <89539031+Soyeon-Cha@users.noreply.github.com>
2025-05-29 20:52:25 +0900

    Create 4_J.cpp

---
+++ week4/4_J.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 세로나 가로 둘 중 하나 길이는 1
+// n 자리 수의 합 최대
+
+int n, m, a[4][4], ans;  // 세로 n, 가로 m
+
+int main(){
+    cin >> n >> m;
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            scanf("%1d", &a[i][j]);
+        }
+    }
+    
+    for(int s=0; s<(1<<(n*m)); s++){
+        int sum = 0;
+
+
+        for(int i=0; i<n; i++){
+            int cur = 0;
+
+            
+            for(int j=0; j<m; j++){
+                int k = i*m + j;
+
+                if((s & (1<<k)) == 0){
+                    cur = cur * 10 + a[i][j];
+                }
+                else{
+                    sum += cur;
+                    cur = 0;
+                }
+            }
+            sum += cur;
+        }
+
+        for(int j=0; j<m; j++){
+            int cur = 0;
+            for(int i=0; i<n; i++){
+                int k = i*m +j;
+                if((s & (1<<k)) != 0){
+                    cur = cur * 10 + a[i][j];
+                }
+                else{
+                    sum += cur;
+                    cur = 0;
+                }
+            }
+            sum += cur;
+        }
+        ans = max(ans, sum);
+    }
+    cout << ans << '\n';
+    return 0;
+}
